06d24ebef78a65848c4f70cba6ed02b672501acc
abyow <77069385+abyow@users.noreply.github.com>
2021-01-06 23:10:21 +0530

    First commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Theme
+    theme: "index",
+
+    // Email notifier account (sender)
+    host: "smtp.mailtrap.io", // Sender email smtp
+    port: "465", // Sender email port
+    user: "zyle.app@gmail.com", // Sender email username
+    pass: "dcmippgxdybamrtt", // Sender email password
+
+    // Your email to receive notification (receiver)  
+    from: '"Contact Me" <zyle.app@gmailcom>', // Sender email address
+    to: 'Abyow1@gmail.com', // Your email address
+    subject: 'Resume Recieved', // Subject
+};
